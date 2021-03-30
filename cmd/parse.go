228507c7c8707c5df53e76e23b8d393fa5de7dd1
Changemin <bkm.change.min@gmail.com>
2021-03-30 15:30:11 +0900

    Upload post

--- cmd/parse.go
+++ cmd/parse.go
@@ -17,13 +17,16 @@ package cmd
 
 import (
 	"fmt"
+	"net/http"
+	"strconv"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/spf13/cobra"
 )
 
 // parseProblemCmd represents the parseProblem command
-var parseProblemCmd = &cobra.Command{
-	Use:   "parseProblem",
+var parseCmd = &cobra.Command{
+	Use:   "parse",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -32,12 +35,12 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("parseProblem called")
+		fmt.Println("parse called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(parseProblemCmd)
+	rootCmd.AddCommand(parseCmd)
 
 	// Here you will define your flags and configuration settings.
 
@@ -48,4 +51,61 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// parseProblemCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// printParse()
+	parseProblem(1000)
+}
+
+func parseProblem(num int) {
+	response, err := http.Get("https://www.acmicpc.net/problem/1000")
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(response.Body)
+
+	probTitle := doc.Find("#problem_title").Text()
+	fmt.Println(probTitle)
+
+	probQuestion := doc.Find("#problem_description").Text()
+	fmt.Println(probQuestion)
+
+}
+
+type Problem struct {
+	title       string
+	description string
+	input       string
+	output      string
+	timeLimit   string
+	memoryLimit string
+	passRatio   string
+}
+
+func printParse() {
+	resp, err := http.Get("https://www.naver.com/")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+
+	// title 태그의 적힌 text를 출력
+	title := doc.Find("title").Text()
+	fmt.Println(title)
+
+	// html tag의 id가 news_cast인 태그의 text를 출력
+	news_cast := doc.Find("#news_cast").Text()
+	fmt.Println(news_cast)
+
+	// div중 3번째 있는 div가져와 text 출력
+	div_three, _ := doc.Find("div").Eq(2).Html()
+	fmt.Println(div_three)
+
+	// 함수를 전달해 사용할 수도 있다. 모든 div의 번호와 내용을 출력
+	doc.Find("div").Each(func(i int, s *goquery.Selection) {
+		fmt.Println(strconv.Itoa(i) + "번째 div" + s.Text())
+	})
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module bj
 go 1.15
 
 require (
+	github.com/PuerkitoBio/goquery v1.6.1
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
