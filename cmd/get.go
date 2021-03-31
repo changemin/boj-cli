3866fd7d6951ce33e9f25b6eca1cad4c90372e6e
Changemin <bkm.change.min@gmail.com>
2021-03-31 12:00:09 +0900

    Improve 'get command' to 'get multiple prob'

--- cmd/get.go
+++ cmd/get.go
@@ -25,37 +25,40 @@ func init() {
 }
 
 func parseProblem(args []string) {
-	if len(args) == 0 {
+	if len(args) == 0 { // 문제 번호 입력을 안했을 경우
+		fmt.Printf(Green, "문제 번호를 입력해주세요\n\nbj get [문제번호]")
 		os.Exit(1)
 	}
-	num, err := strconv.Atoi(args[0])
+	for idx, strProbNum := range args {
+		num, err := strconv.Atoi(strProbNum)
 
-	if err != nil {
-		fmt.Println("문제 번호를 입력해주세요")
+		if err != nil {
+			fmt.Println("문제 번호를 입력해주세요")
+		}
+		prob := Problem{num: num}
+
+		url := "https://www.acmicpc.net/problem/" + strProbNum
+		response, err := http.Get(url)
+		if err != nil {
+			panic(err)
+		}
+		defer response.Body.Close()
+
+		doc, err := goquery.NewDocumentFromReader(response.Body)
+
+		fmt.Println(strconv.Itoa(idx) + "Prob num : " + strconv.Itoa(prob.num))
+		prob.title = doc.Find("#problem_title").Text()
+		fmt.Println("title :\n" + prob.title)
+
+		prob.description = strings.TrimLeft(doc.Find("#problem_description").Text(), " ")
+		fmt.Println("description :\n" + prob.description)
+
+		prob.input = doc.Find("#sample-input-1").Text()
+		fmt.Println("input :\n" + prob.input)
+
+		prob.output = doc.Find("#sample-output-1").Text()
+		fmt.Println("output :\n" + prob.output)
 	}
-
-	prob := Problem{num: num}
-
-	url := "https://www.acmicpc.net/problem/" + args[0]
-	response, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer response.Body.Close()
-
-	doc, err := goquery.NewDocumentFromReader(response.Body)
-
-	prob.title = doc.Find("#problem_title").Text()
-	fmt.Println("title : " + prob.title)
-
-	prob.description = strings.TrimLeft(doc.Find("#problem_description").Text(), " ")
-	fmt.Println("description : " + prob.description)
-
-	prob.input = doc.Find("#sample-input-1").Text()
-	fmt.Println("input : " + prob.input)
-
-	prob.output = doc.Find("#sample-output-1").Text()
-	fmt.Println("output : " + prob.output)
 
 	// TODO: - table 파싱
 }
@@ -71,3 +74,14 @@ type Problem struct {
 	// memoryLimit string
 	// passRatio   string
 }
+
+const (
+	Black   = "\033[1;30m%s\033[0m"
+	Red     = "\033[1;31m%s\033[0m"
+	Green   = "\033[1;32m%s\033[0m"
+	Yellow  = "\033[1;33m%s\033[0m"
+	Purple  = "\033[1;34m%s\033[0m"
+	Magenta = "\033[1;35m%s\033[0m"
+	Teal    = "\033[1;36m%s\033[0m"
+	White   = "\033[1;37m%s\033[0m"
+)
